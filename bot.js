88ff4e739659a32d29c0c45444892d6b53b40b61
arielturchak <arielgit config --global user.email arielturchak@hotmail.comgit config --global user.email ariel>
2024-02-12 19:58:03 +0100

    segundo commit "comentarios"

--- bot.js
+++ bot.js
@@ -1,27 +1,35 @@
+// Importa las bibliotecas necesarias
 const {Telegraf} = require('telegraf');
 const axios = require('axios');
+// Importa las constantes desde el archivo data.js
 const {tokentiempo, tokentelegram, headers} = require('./data.js');
-module.exports = {tokentiempo, tokentelegram, headers};
 
+// Crea una nueva instancia de Telegraf utilizando el token de Telegram
 const bot = new Telegraf(tokentelegram);
-https://history.openweathermap.org/data/2.5/history/city?lat=41.85&lon=-87.65&appid=ae22f43aba7ad10eb86dd2e369819e7a
 
+// Cuando el bot recibe el comando /start, envía un mensaje de bienvenida
 bot.start((ctx) => ctx.reply('Bienvenido a tu bot de clima!'));
+
+// Cuando el bot escucha el mensaje 'clima', solicita al usuario que ingrese el nombre de una ciudad
 bot.hears('clima', (ctx) => {
     ctx.reply('Ingresa el nombre de la ciudad para obtener el clima');
 });
 
+// Cuando el bot recibe un mensaje de texto (que se supone que es el nombre de una ciudad), realiza una solicitud GET a la API de OpenWeatherMap
 bot.on('text', (ctx) => {
     let city = ctx.message.text;
     axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tokentiempo}`, {headers})
         .then(response => {
-            let temp = response.data.main.temp - 273.15; // Convert from Kelvin to Celsius
+            // Convierte la temperatura de Kelvin a Celsius y envía un mensaje con la temperatura actual en la ciudad especificada
+            let temp = response.data.main.temp - 273.15;
             ctx.reply(`El clima en ${city} es de ${temp.toFixed(2)}°C`);
         })
         .catch(error => {
+            // Si ocurre un error durante la solicitud a la API, registra el error y envía un mensaje de error
             console.error(`Error: ${error}`);
             ctx.reply('Ha ocurrido un error al obtener el clima');
         });
 });
 
+// Inicia el bot
 bot.launch();
