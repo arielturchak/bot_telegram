66ca32000fa63549042e8ec36400b656754e0719
arielturchak <arielgit config --global user.email arielturchak@hotmail.comgit config --global user.email ariel>
2024-02-13 16:48:39 +0100

    quinto commit "documentación con js.doc "

--- data.js
+++ data.js
@@ -4,5 +4,5 @@ const headers ={
     "X-Auth-Token":"c5d0aa0bb500451d992a2966d1f5e958",
 }
 const tokentiempo="ae22f43aba7ad10eb86dd2e369819e7a";
-
-module.exports = {tokentelegram, tokentiempo,headers } 
+const tokennoticias="7cc6b29c5ab64a79961bc5b2887b1ab7";
+module.exports = {tokentelegram, tokentiempo,headers,tokennoticias} 
